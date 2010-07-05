096fb3235193605479461a3e8823f5bba05513b1
cc8778 <cc8778@sohu.com>
2010-07-05 13:04:09 +0000

    1：增减Download项目 2：消除各个目录下不需要的文件夹

---
+++ MusicDownload/DownloadMusic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CMusicSearch.MusicDownload
+{
+    /// <summary>
+    /// 下载音乐文件
+    /// </summary>
+    class DownloadMusic
+    {
+        public Stream 
+
+    }
+}
